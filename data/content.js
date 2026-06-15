3ee19806a24b783f84a1a370025fcdafbef94823
Gerson Henriquez Avila <u21206891@utp.edu.pe>
2026-06-15 12:02:14 -0500

    Actualización data

--- data/content.js
+++ data/content.js
@@ -8,18 +8,18 @@ window.GOPERU_DATA = {
     {
       "game": "StarCraft II",
       "status": "Automático",
-      "startISO": "2026-06-15T06:14",
-      "durationMin": 180,
+      "startISO": "2026-06-14T20:00",
+      "durationMin": 120,
       "name": "Clan Wars -3500MMR",
       "rival": "[OsR] Old School Reborn",
       "result": "Campeones, Gersbot y Deathwork",
       "score": "4-0",
       "stream": "https://www.twitch.tv/videos/2796945096",
-      "challonge": "https://challonge.com/es/2cg5oxyz?fbclid=IwY2xjawScKV5leHRuA2FlbQIxMABicmlkETFhclF2Sk1kb1NQNVNyUjlEc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHlERBgHFdRkEwGasQQcdaR2QG7YiGAZjUq8QePdaKV0T_Mgq637iFX09F0h9_aem_5NUOnlWZahA_UzCwmfyIPw",
+      "challonge": "",
       "desc": "Torneo de -3500mmr registrado en la pagina oficial de GoPeru",
       "hidden": false,
-      "date": "15 JUN 2026",
-      "time": "06:14"
+      "date": "14 JUN 2026",
+      "time": "20:00"
     }
   ],
   "roster": {
